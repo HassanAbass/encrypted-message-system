4e111321e89f2fc6fe2532efabdb4cb8a54f4294
hab <hab@luftborn.com>
2024-04-27 04:23:02 +0200

    add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Sender User',
+            'email' => 'sender@example.com',
             'password' => Hash::make('123456'),
         ]);
         User::factory()->create([
-            'name' => 'recipient User',
+            'name' => 'Recipient User',
             'email' => 'recipient@example.com',
             'password' => Hash::make('123456'),
         ]);
